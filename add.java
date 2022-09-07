5539f707906dd03830e50f3608a62fac74ae55a8
Chandan Soni <90852214+Chandansoni2021@users.noreply.github.com>
2022-09-07 14:12:04 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class add{
+    public static void main(String args[]) {
+        int a=0;
+        System.out.println(a);
+       // Scanner sc=new Scanner(System.in);
+        //System.out.println("pleasee enter the value of a ");
+
+
+//       String  a = sc.next();
+//       System.out.println("plesase enter the value of b");
+//       int b=sc.nextInt();
+//     //   int sum=a+b;
+//       System.out.println("sum= "+a);
+        
+//     }
+// }
+
+
+
+// float radius=3.2f ;
+// float area = 0;  
+// final float pi =  3.14f;     //it is final var not mathod 
+// area = pi*radius*radius;
+// System.out.println(area);
+    }
+ }
+
