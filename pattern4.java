c7037987aee2917d8fc61a1eec0424882fa40950
Chandan Soni <90852214+Chandansoni2021@users.noreply.github.com>
2022-10-19 16:32:44 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,18 @@
+class pattern4{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j =0;j<2*n-1;j){
+                if(j<i &&j<n-i){
+                    System.out.print("*");
+
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
