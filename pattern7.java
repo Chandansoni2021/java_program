2c895b70cbbb1a556064447349a6a42d82a3d655
Chandan Soni <90852214+Chandansoni2021@users.noreply.github.com>
2022-10-19 23:31:31 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class pattern7{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j =1;j<=n;j++){
+                if(j>n-1-i){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
+//         * 
+//       * * 
+//     * * * 
+//   * * * * 
+// * * * * *
